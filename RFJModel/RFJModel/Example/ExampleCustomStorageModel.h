00f92c3f822e16df7d930deb1afb4d54b28bdcfb
hzgouzhehua <gzh@GZHdeMacBook-Pro.local>
2017-06-29 14:27:34 +0800

    更新工程文件

---
+++ RFJModel/RFJModel/Example/ExampleCustomStorageModel.h
@@ -0,0 +1,14 @@
+//
+//  ExampleCustomStorageModel.h
+//  RFJModel
+//
+//  Created by GZH on 2017/6/29.
+//  Copyright © 2017年 TechAtk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "RFJModel.h"
+
+@interface ExampleCustomStorageModel : RFJModel
+
+@end
---
+++ RFJModel/RFJModel/Example/ExampleCustomStorageModel.m
@@ -0,0 +1,13 @@
+//
+//  ExampleCustomStorageModel.m
+//  RFJModel
+//
+//  Created by GZH on 2017/6/29.
+//  Copyright © 2017年 TechAtk. All rights reserved.
+//
+
+#import "ExampleCustomStorageModel.h"
+
+@implementation ExampleCustomStorageModel
+
+@end
